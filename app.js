95b92f4e64caa383ddac3aab777e04a23059bf88
rihwan <rihwan@gmail.com>
2020-08-10 21:12:20 -0700

    Adding stylesheet in file list.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 
@@ -62,17 +63,67 @@ var video_files = retrieveFiles(video_dir);
 console.log('# of videos:' + video_files.length);
 
 app.get('/', function (req, res, next) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var content = '<html><head><title>Local Video Streaming</title></head><body>';
+    var content = '';
+    content += '<html>\n';
+    content += '  <head>\n';
+    content += '    <meta charset="UTF-8">\n';
+    content += '    <title>Local Video Streaming</title>\n';
+    content += '    <link rel="stylesheet" type="text/css" href="';
+    content += 'http://' + hostname + ':' + port + '/css/style.css" />\n';
+    content += '  </head>\n';
+    content += '  <body>\n';
+    content += '    <div id="file_list">\n';
+    content += '      <ul id="ann">\n';
     for (var i = 0; i < video_files.length; ++i) {
-        var curr_content = '<a href="http://';
-        curr_content += hostname + ':' + port + '/video/?index=' + i + '">';
-        curr_content += i + ': ' + video_files[i].relative_path + '</a><br />';
-        content += curr_content;
+        content += '      <li><a href="http://';
+        content += hostname + ':' + port;
+        content += '/video_player/?index=' + i + '">';
+        content += i + ': ' + video_files[i].relative_path + '</a></li>\n';
     }
-    content += '</body></html>';
+    content += '      </ul>\n';
+    content += '    </div>\n';
+    content += '  </body>\n';
+    content += '</html>\n';
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(content);
     res.end();
+});
+
+app.get('/video_player', function (req, res, next) {
+    var index = -1;
+    if (req.query && req.query.index) {
+        index = parseInt(req.query.index);
+    }
+
+    if (index >= 0 && index < video_files.length) {
+        var content = '';
+        content += '<html>\n';
+        content += '  <head>\n';
+        content += '    <meta charset="UTF-8">\n';
+        content += '    <title>' + video_files[index].relative_path + '</title>\n';
+        content += '    <link rel="stylesheet" type="text/css" href="';
+        content += 'http://' + hostname + ':' + port + '/css/style.css" />\n';
+        content += '  </head>\n';
+        content += '  <body>\n';
+        content += '    <center>\n';
+        content += '      <video id="videoPlayer" controls>\n';
+        content += '        <source src="http://' + hostname + ':' + port;
+        content += '/video/?index=' + index + '" type="';
+        content += video_files[index].type + '">\n';
+        content += '      </video>\n';
+        content += '    </center>\n';
+        content += '  </body>\n';
+        content += '</html>\n';
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(content);
+        res.end();
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write('Unknown video file');
+        res.end();
+    }
 });
 
 app.get('/video', function (req, res, next) {
